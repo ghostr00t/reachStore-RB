43827b2e6d7674e685520de7a637092e8fb46d09
Travis Wise <richardtwise@gmail.com>
2017-06-22 00:30:17 -0600

    added various styles and started building contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   get 'about' => 'pages#about', as: :about
   get 'contact' => 'pages#contact', as: :contact
+  get 'contact-me', to: 'messages#new', as: 'new_message'
+  post 'contact-me', to: 'messages#create', as: 'create_message'
+
 
   root to: 'listings#index'
-  resources :listings
+  resources :listings, :messages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
